ffc7f629445f3c9f0247c150546a3ea7dfa9b347
Dawid Ryłko <dawid.rylko@gmail.com>
2020-10-03 16:03:12 +0200

    Generate snippet module and prepare form

---
+++ src/app/snippet/component/snippet-form/snippet-form.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SnippetFormComponent } from './snippet-form.component';
+
+describe('src/app/snippet/component/snippet-form', () => {
+  let component: SnippetFormComponent;
+  let fixture: ComponentFixture<SnippetFormComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [SnippetFormComponent],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SnippetFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
